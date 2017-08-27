c88997c8e5100b3540dee21fb210c2cc78cf5d8b
Mikey <superm0419@gmail.com>
2017-08-27 19:02:13 -0400

    Tutorial ;)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,11 @@ export class Hero {
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
     </ul>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div><label>name: </label>{{hero.name}}</div>
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>id: </label>{{selectedHero.id}}</div>
   <div>
 <label>name: </label>
-<input [(ngModel)]="hero.name" placeholder="name">
+<input [(ngModel)]="selectedHero.name" placeholder="name">
 </div>`,
   // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
@@ -77,9 +76,12 @@ export class Hero {
 })
 export class AppComponent {
   title = "Tour of heroes";
- 
   selectedHero: Hero;
   heroes = HEROES;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 
 
 }
